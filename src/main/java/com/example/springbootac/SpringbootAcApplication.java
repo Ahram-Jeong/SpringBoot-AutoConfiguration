db6b58ab840f8f6930061642c549df2e4121bcb2
Ahram-Jeong <ahram1215@gmail.com>
2023-12-15 21:29:00 +0900

    20231215 - 자동 구성 조건 결과 확인

--- src/main/java/com/example/springbootac/SpringbootAcApplication.java
+++ src/main/java/com/example/springbootac/SpringbootAcApplication.java
@@ -11,16 +11,20 @@ public class SpringbootAcApplication {
     @Bean
     ApplicationRunner run(ConditionEvaluationReport report) {
         return args -> {
-            report.getConditionAndOutcomesBySource().entrySet().stream()
-                    .filter(co -> co.getValue().isFullMatch())
-                    .filter(co -> co.getKey().indexOf("Jmx") < 0)
-                    .forEach(co -> {
-                        System.out.println(co.getKey());
-                        co.getValue().forEach(c -> {
-                            System.out.println("\t" + c.getOutcome());
-                        });
-                        System.out.println();
-                    });
+            System.out.println(
+                report.getConditionAndOutcomesBySource().entrySet().stream()
+                        .filter(co -> co.getValue().isFullMatch())
+                        .filter(co -> co.getKey().indexOf("Jmx") < 0)
+                        .map(co -> {
+                            System.out.println(co.getKey());
+                            co.getValue().forEach(c -> {
+                                System.out.println("\t" + c.getOutcome());
+                            });
+                            System.out.println();
+                            return co;
+                        }).count()
+            );
+
         };
     }
 
